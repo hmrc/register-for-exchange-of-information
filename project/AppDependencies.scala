b16afc3ed62f068dc23fa6fb35bc2396fe24f6ff
Shailesh Patil <shaileshp@gmail.com>
2022-07-26 14:25:24 +0200

    DAC6-1991-Upgrade-Mongo (#29)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,13 +7,13 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.24.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.62.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.68.0",
     "uk.gov.hmrc" %% "domain" % "8.0.0-play-28"
   )
 
   val test = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.8.0" % Test,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % "0.52.0" % Test,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % "0.68.0" % Test,
     "org.mockito" %% "mockito-scala" % "1.16.42" % Test,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % Test,
     "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2" % Test,
