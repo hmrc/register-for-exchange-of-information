87e54cd00894f3ec2ab14c8ac056bb27d552ffd4
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2023-03-01 13:41:36 +0000

    DAC6-2333: Address PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,24 +5,27 @@ import sbt._
 
 object AppDependencies {
 
+  private val bootstrapVersion = "7.13.0"
+  private val mongoVersion = "0.73.0"
+
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "7.11.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.73.0",
-    "uk.gov.hmrc" %% "domain" % "8.0.0-play-28"
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % mongoVersion,
+    "uk.gov.hmrc"       %% "domain"                     % "8.0.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.21.0" % Test,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % "0.68.0" % Test,
-    "org.mockito" %% "mockito-scala" % "1.16.42" % Test,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % Test,
-    "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2" % Test,
-    "wolfendale" %% "scalacheck-gen-regexp" % "0.1.2" % Test,
-    "org.jsoup" % "jsoup" % "1.12.1" % Test,
-    "org.scalatest" %% "scalatest" % "3.1.0" % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test,
-    "com.github.tomakehurst" % "wiremock-standalone" % "2.27.0" % Test,
-    "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it"
-  )
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % mongoVersion,
+    "org.mockito"             %% "mockito-scala"              % "1.16.42",
+    "com.typesafe.play"       %% "play-test"                  % PlayVersion.current,
+    "org.scalatestplus"       %% "scalatestplus-scalacheck"   % "3.1.0.0-RC2",
+    "wolfendale"              %% "scalacheck-gen-regexp"      % "0.1.2",
+    "org.jsoup"                % "jsoup"                      % "1.12.1",
+    "org.scalatest"           %% "scalatest"                  % "3.1.0",
+    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0",
+    "com.github.tomakehurst"   % "wiremock-standalone"        % "2.27.0",
+    "com.vladsch.flexmark"     % "flexmark-all"               % "0.36.8"
+  ).map(_ % "test, it")
 
 }
