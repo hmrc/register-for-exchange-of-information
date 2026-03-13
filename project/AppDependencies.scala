240677c3c5f39c1db9bdead2b1b83e65c46102d3
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2026-03-13 13:45:00 +0000

    PR Bot comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
-  private val mongoVersion     = "2.11.0"
+  private val mongoVersion     = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
