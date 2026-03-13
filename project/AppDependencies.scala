9b19e61a204eb816271ab03dbd703728995ed667
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2026-03-13 14:25:28 +0000

    DDCYLS-8142: dependency upgrades to fix vulnerabilities (#46)
    * dependency upgrades to fix vulnerabilities
    
    * PR Bot comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
-  private val mongoVersion     = "2.11.0"
+  private val bootstrapVersion = "10.7.0"
+  private val mongoVersion     = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
